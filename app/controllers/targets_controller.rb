5277ac9a829eb3f91370ca7b51b978e82960f974
Sayuj Othayoth <sayuj@othayoth.in>
2015-07-13 22:49:23 +0530

    Add UI

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -1,5 +1,14 @@
 class TargetsController < ApplicationController
+  before_action :set_target, only: [:show]
+
   def index
     @targets = Target.all
+  end
+
+  def show
+  end
+
+  def set_target
+    @target = Target.find(params[:id])
   end
 end
