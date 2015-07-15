1291fd684c7150489f94c7a30e832eb3f6223521
Sayuj Othayoth <sayuj@qburst.com>
2015-07-15 17:44:15 +0530

    Refactor target updator spec

--- spec/target/target_updater_spec.rb
+++ spec/target/target_updater_spec.rb
@@ -1,16 +1,18 @@
 require 'spec_helper'
 
 describe TargetUpdater do
-  # TODO: Rewrite
   describe '#update_reachability' do
-    let(:target) { create(:target) }
+    let(:target1) { create(:target) }
+    let(:target2) { create(:target) }
 
     before do
-      allow(Target).to receive(:all) { [target] }
+      allow(Target).to receive(:all) { [target1, target2] }
     end
 
     it 'updates reachablity of each targets' do
-      expect(target).to receive(:update_reachability!)
+      expect(target1).to receive(:update_reachability!)
+      expect(target2).to receive(:update_reachability!)
+
       subject.update_reachability
     end
   end
